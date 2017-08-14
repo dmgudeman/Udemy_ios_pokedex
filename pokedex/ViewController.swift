c1f7b35ad657421c9cff1a02976025481a5b8a9c
davidmgudeman <davidmgudeman@gmail.com>
2017-08-14 16:53:38 -0700

    added custom fonts and music

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
   
   @IBOutlet weak var collection: UICollectionView!
   
   var pokemon = [Pokemon]()
+  var musicPlayer:AVAudioPlayer!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,7 +22,20 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     collection.dataSource = self
     collection.delegate = self
     parsePokemonCSV()
+    initAudio()
     
+  }
+  func initAudio() {
+    let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+    do {
+      musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+      musicPlayer.prepareToPlay()
+      musicPlayer.numberOfLoops = -1
+      musicPlayer.play()
+      
+    } catch let err as NSError {
+      print(err.debugDescription)
+    }
   }
   
   func parsePokemonCSV() {
@@ -75,6 +90,19 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return CGSize(width: 105, height: 105)
   }
+
+  @IBAction func musicBtnPressed(_ sender: UIButton) {
+    if musicPlayer.isPlaying {
+      musicPlayer.pause()
+      sender.alpha = 0.2
+      
+    } else {
+      musicPlayer.play()
+      sender.alpha = 1.0
+    
+    }
+  }
+
 }
 
 
