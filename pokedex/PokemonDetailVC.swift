12ee4e45168b7c0e333044f1dd57a05b4d513b71
davidmgudeman <davidmgudeman@gmail.com>
2017-08-15 08:24:18 -0700

    added all the outlets for Details Page

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
   
-
+  @IBOutlet weak var mainImg: UIImageView!
+  @IBOutlet weak var descriptionLbl: UILabel!
+  @IBOutlet weak var typeLbl: UILabel!
+  @IBOutlet weak var defenseLbl: UILabel!
+  @IBOutlet weak var heightLbl: UILabel!
+  @IBOutlet weak var pokeIdLbl: UILabel!
+  @IBOutlet weak var weightLbl: UILabel!
+  @IBOutlet weak var attackLbl: UILabel!
+  @IBOutlet weak var currentEvoImg: UIImageView!
+  @IBOutlet weak var nextEvoImg: UIImageView!
+  @IBOutlet weak var evoLbl: UILabel!
   
-    var pokemon: Pokemon!
+  
+  var pokemon: Pokemon!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +32,9 @@ class PokemonDetailVC: UIViewController {
     }
 
    
+  @IBAction func backBtnPressed(_ sender: AnyObject) {
+    dismiss(animated: true, completion: nil)
+  }
 
 }
  
