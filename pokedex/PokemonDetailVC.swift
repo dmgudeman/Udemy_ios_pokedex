543d6c11a94a978451d4e8dd70b54c1bc5644cbf
davidmgudeman <davidmgudeman@gmail.com>
2017-08-14 19:09:10 -0700

    styled the details page

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,26 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by David Gudeman on 8/14/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+  
+
+  
+    var pokemon: Pokemon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+   
+
+}
+ 
